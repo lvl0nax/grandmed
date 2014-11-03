e82ab25c2cadab8b7ad34a3fadaa7ced6fdb2057
lvl0nax <lvl0nax@gmail.com>
2014-11-03 23:43:40 +0300

    try to fix cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-
+env :PATH, ENV['PATH']
 # Example:
 #
 set :output, 'cron_log.log'
